701f549ff5d640fe7b22bac032047d4c87d3e2a0
Xiaoxiang <wangxiaoxiang@youku.com>
2015-08-21 14:17:36 +0800

    f

--- CPP/OperatorOverloading/friend.cpp
+++ CPP/OperatorOverloading/friend.cpp
@@ -5,10 +5,11 @@ using namespace std;
 class Complex
 {
 public:
-	Complex(double m_a,double m_b):m_a(m_a),m_b(m_b){};
+	Complex(double m_a = 0.0f,double m_b = 0.0f):m_a(m_a),m_b(m_b){};
 private:
 	friend istream& operator >> (istream& is, Complex&);
 	friend ostream& operator << (ostream& os, const Complex&);
+    friend class Sum;
 	double m_a,m_b;
 };
 
@@ -29,8 +30,8 @@ class Sum
 public:
 	Sum(const Complex& c1,const Complex& c2):c1(c1),c2(c2)
 	{
-		Complex cs(c1.m_a+c2.m_a,c1.m_b+c2.m_b);
-		this->cs = cs;
+        cs.m_a = c1.m_a + c2.m_a;
+        cs.m_b = c1.m_b + c2.m_b;
 	};
 	void show()
 	{
@@ -40,7 +41,7 @@ private:
 	Complex cs;
 	Complex c1;
 	Complex c2;
-}
+};
 
 void test_operator()
 {
