bdede838920beccb9b117ad960149d46fe468533
Xiaoxiang <wangxiaoxiang@youku.com>
2015-09-10 16:43:42 +0800

    多态01

---
+++ CPP/Inheritance and polymorphism/Polymorphism01.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Animal
+{
+public:
+  void howl() const
+  {
+    cout<<"Animal howl……"<<endl;
+  }
+};
+
+class Wofl:public Animal
+{
+public:
+  void howl() const
+  {
+    cout<<"Wofl howl(howl howl),吓死宝宝了。"<<endl;
+  }
+};
+
+class Cat:public Animal
+{
+public:
+  void howl() const
+  {
+    cout<<"Cat howl(miao miao)"<<endl;
+  }
+};
+
+void AnimalHowl(const Animal& animal)
+{
+  animal.howl();
+}
+
+int main(int argv,char** argc)
+{
+  Wofl wofl;
+  Cat cat;
+
+
+  AnimalHowl(wofl);
+  //output Animal howl……
+
+  AnimalHowl(cat);
+  //output Animal howl……
+  return 0;
+}
